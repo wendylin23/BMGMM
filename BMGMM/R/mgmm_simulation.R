0aaba9821317ec1401ef38b57e3c236a4e75c646
Wenyi Lin <linwenyi23@gmail.com>
2021-01-21 13:37:21 -0800

    MGMM simulation updated

--- BMGMM/R/mgmm_simulation.R
+++ BMGMM/R/mgmm_simulation.R
@@ -7,7 +7,6 @@ mgmm_simulation <- function(data.sim,pars_true,X,Z,plot=FALSE,
   beta_true = pars_true[[1]]
   alpha_true = pars_true[[2]]
   gamma_true = pars_true[[3]]
-  #A_true = pars_true[[4]]
   Sigma_a_true = pars_true[[5]]
   s_sq_eps_true = pars_true[[6]]
   
@@ -20,13 +19,11 @@ mgmm_simulation <- function(data.sim,pars_true,X,Z,plot=FALSE,
     pi_true = rbind(pi_true,pi_i)
     K_i = which(rmultinom(1, size=1, prob=pi_i)==1)
     K_true = c(K_true,K_i)
-    #A_true[i,] = rmvnorm(1,rep(0,2*P),Sigma_a_true[,,K_i])
     A_true[i,] = rmvnorm(1,alpha_true[,K_i],Sigma_a_true[,,K_i])
     for(p in 1:P)
     {
       data.sim$K[data.sim$id == i&data.sim$outcome==p] = K_i
       T_i = cbind(1,data.sim$age[data.sim$id == i&data.sim$outcome==p])
-      #A_true[i,(2*p-1):(2*p)] = A_true[i,(2*p-1):(2*p)] + cbind(alpha_true[(2*p-1):(2*p),K_i])
       data.sim$y[data.sim$id == i&data.sim$outcome==p] = X[[i]]%*%beta_true[,p] + T_i%*%(cbind(A_true[i,(2*p-1):(2*p)])) + rnorm(TT,0,s_sq_eps_true^.5)
     }
   }
@@ -61,6 +58,7 @@ mgmm_simulation <- function(data.sim,pars_true,X,Z,plot=FALSE,
   ## format data for input ##
   ###########################
   ###########################
+  
   Y_sparse=list() # list for saving multivariate outcomes
   time_sparse=list() # list for saving time in random effects
   M_i_sparse = list() # list for saving number of inputs for each visit by outcome
